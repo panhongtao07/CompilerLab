222244831eb2338a2f324da742ff34768e2010c9
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2023-01-16 22:25:14 +0800

    3 表达式与值的定义

---
+++ src/AST_SysY.cpp
@@ -0,0 +1,39 @@
+#include "AST_SysY.h"
+
+// 全局变量
+
+// 初始化类静态成员
+const std::string BinaryAST::type_str[] = {
+    "UNDEF",
+    // 算术运算
+    "ADD", "SUB", "MUL", "DIV", "MOD",
+    // 逻辑运算
+    "AND", "OR", "XOR",
+    // 比较运算
+    "EQ", "NE", "LT", "GT", "LTE", "GTE",
+};
+const std::string BinaryAST::inst_str[] = {
+    "_undef_",
+    // 算术运算
+    "add", "sub", "mul", "div", "mod",
+    // 逻辑运算
+    "and", "or", "xor",
+    // 比较运算
+    "eq", "ne", "lt", "gt", "le", "ge",
+};
+const std::string BinaryAST::op_str[] = {
+    "_undef_",
+    // 算术运算
+    "+", "-", "*", "/", "%",
+    // 逻辑运算
+    "&", "|", "^",
+    // 比较运算
+    "==", "!=", "<", ">", "<=", ">=",
+};
+
+
+void ExpAST::set_value_as_temp() {
+    if (is_value()) return;
+    if (set_value(new ValueAST())) return;
+    assert(false);
+}
--- src/AST_SysY.h
+++ src/AST_SysY.h
@@ -4,6 +4,28 @@
 // 不写IDE报错很多，太难看了，还是写上吧
 #include <iostream>
 #include <memory>
+#include <cassert>
+
+#define prepare_expr(expr) \
+    do { \
+        if (!(expr)->is_value()) { \
+            o << *(expr); \
+        } \
+    } while (0)
+
+#define define_expr_with_set_method(expr, method_name) \
+    std::shared_ptr<ExpAST> expr; \
+    void method_name(ExpAST* expr) { \
+        expr->set_value_as_temp(); \
+        this->expr = std::shared_ptr<ExpAST>(expr); \
+    }
+
+
+inline int var_count = 0;
+
+class BaseAST;
+class ExpAST;
+class ValueAST;
 
 // 所有 AST 的基类
 class BaseAST {
@@ -71,14 +93,169 @@ public:
     }
 };
 
-class StmtAST : public BaseAST {
+// 表达式的基类
+class ExpAST: public BaseAST {
 public:
+    // 该类为单个值还是表达式
+    virtual bool is_value() const {return false;}
+    /* 设置表达式结果值为v，返回是否成功（可用于避免临时中间变量生成）
+       如果成功，应删除赋值语句
+       例如：语句 Identifier = Exp; 可实现为
+       return Assign(Identifier, Exp);
+       若能成功设置结果，可实现为
+       if (Exp->set_value(Identifier)) return Exp;
+    */
+    virtual bool set_value(const ValueAST* v) {return false;}
+    // 设置表达式结果值为v, 单个值不做任何操作
+    // 必须成功，否则报错
+    void set_value_as_temp();
+    // 获取表达式结果值
+    virtual const ValueAST* value_ptr() const = 0;
+};
+
+// 单值或符号
+class ValueAST : public ExpAST {
+public:
+    enum class Type {
+        // 默认值：未定义
+        Undef,
+        Num,
+        Var,
+    } type;
     int number;
+
+    ValueAST() : type(Type::Var), number(var_count++) {}
+    ValueAST(int number) : type(Type::Num), number(number) {}
+
     std::ostream& dump(std::ostream& o = std::cout) const override {
-        return o << "return " << number << ";";
+        switch (type) {
+            case Type::Num:
+                return o << number;
+            case Type::Var:
+                return o << "V {" << number << "}";
+            default:
+                assert(false);
+        }
     }
     std::ostream& compile(std::ostream& o = std::cout) const override {
-        return o << "ret " << number << std::endl;
+        switch (type) {
+            case Type::Num:
+                return o << number;
+            case Type::Var:
+                return o << "%" << number;
+            default:
+                assert(false);
+        }
+    }
+
+    bool is_value() const override {
+        return true;
+    }
+
+    const ValueAST* value_ptr() const override {
+        return this;
     }
 };
+
+// 二元运算
+class BinaryAST : public ExpAST {
+public:
+    // SysY 语言支持的运算符不包含位运算
+    enum class Type {
+        // 默认值：未定义
+        Undef,
+        // 算术运算
+        Add,
+        Sub,
+        Mul,
+        Div,
+        Mod,
+        // 逻辑运算
+        And,
+        Or,
+        Xor,
+        // 比较运算
+        Eq,
+        Ne,
+        Lt,
+        Gt,
+        Lte,
+        Gte,
+    } type;
+    const static std::string type_str[];
+    const static std::string inst_str[];
+    const static std::string op_str[];
+
+    define_expr_with_set_method(l_val, set_left)
+    define_expr_with_set_method(r_val, set_right)
+    std::shared_ptr<ValueAST> result;
+
+    // 在编译前, 由语义分析器设置包含结果在内的所有信息
+    BinaryAST() : type(Type::Undef) {}
+
+    const std::string& get_type_str() const {
+        return type_str[(int)type];
+    }
+
+    const std::string& get_inst_str() const {
+        return inst_str[(int)type];
+    }
+
+    std::ostream& dump(std::ostream& o = std::cout) const override {
+        assert(type != Type::Undef);
+        assert(result);
+        return o << get_type_str() << " {" << *l_val << ", " << *r_val << "}";
+    }
+
+    std::ostream& compile(std::ostream& o = std::cout) const override {
+        assert(type != Type::Undef);
+        // 先生成子表达式
+        prepare_expr(l_val);
+        prepare_expr(r_val);
+        // 生成运算指令
+        o << *result << " = " << get_inst_str() << " " << *l_val->value_ptr()
+          << ", " << *r_val->value_ptr() << std::endl;
+        return o;
+    }
+
+    bool set_value(const ValueAST* v) override {
+        if (result) return false;
+        result = std::make_shared<ValueAST>(*v);
+        return true;
+    }
+
+    const ValueAST* value_ptr() const override {
+        return result.get();
+    }
+};
+
+// 语句的基类
+class StmtAST : public BaseAST {
+};
+
+// 返回语句
+class ReturnAST : public StmtAST {
+public:
+    define_expr_with_set_method(expr, set_expr)
+
+    std::ostream& dump(std::ostream& o = std::cout) const override {
+        if (expr) {
+            prepare_expr(expr);
+            o << "return " << *expr->value_ptr() << ";";
+        } else {
+            o << "return;";
+        }
+        return o << "return " << *expr << ";";
+    }
+    std::ostream& compile(std::ostream& o = std::cout) const override {
+        if (expr) {
+            prepare_expr(expr);
+            o << "ret " << *expr->value_ptr() << std::endl;
+        } else {
+            o << "ret" << std::endl;
+        }
+        return o;
+    }
+};
+
 #endif
