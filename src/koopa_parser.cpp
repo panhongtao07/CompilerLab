bb80d8d1e02c0531b484f0eb9649f358b98152ed
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2023-01-20 23:57:11 +0800

    3.b 表达式代码生成

--- src/koopa_parser.cpp
+++ src/koopa_parser.cpp
@@ -1,12 +1,14 @@
 #include <cassert>
 #include <iostream>
 #include <string>
+#include <unordered_map>
 #include "koopa.h"
 #include "koopa_parser.h"
 
 #define Indentation (std::string(indent_level, ' '))
 #define OUT (o << Indentation)
 #define real_name(v) (v->name + 1)
+#define debug(v) (std::cout << v << std::endl)
 
 // 程序
 void KoopaParser::Visit(const koopa_raw_program_t &program) {
@@ -77,6 +79,14 @@ void KoopaParser::Visit(const koopa_raw_value_t &value) {
             // 整数指令
             OUT << "Integer " << kind.data.integer.value << std::endl;
             break;
+        case KOOPA_RVT_BINARY:
+            // 二元指令
+            OUT << "Binary " << kind.data.binary.op << std::endl;
+            indent_level += 2;
+            Visit(kind.data.binary.lhs);
+            Visit(kind.data.binary.rhs);
+            indent_level -= 2;
+            break;
         default:
             // 其他类型暂时遇不到
             assert(false);
@@ -84,6 +94,83 @@ void KoopaParser::Visit(const koopa_raw_value_t &value) {
 }
 
 // 编译器
+
+// 寄存器和栈位置分配
+
+// 获取指令在栈中的偏移量
+int KoopaParser::get_offset(const koopa_raw_value_t &value) {
+    std::cout << value->kind.tag << std::endl;
+    assert(value_map.find(value) != value_map.end() && "value not found");
+    return stack_length - value_map[value];
+}
+
+// 设置指令在栈中的偏移量
+void KoopaParser::set_offset(const koopa_raw_value_t &value) {
+    value_map[value] = stack_length;
+    stack_length += 4;
+}
+
+// 遍历记录偏移量
+
+// 指令
+void KoopaParser::record_offset(const koopa_raw_value_t &value) {
+    // 无论是否有返回值, 都记录4字节的偏移量即可
+    set_offset(value);
+}
+
+// 基本块
+void KoopaParser::record_offset(const koopa_raw_basic_block_t &bb) {
+    // 访问所有指令
+    auto &slice = bb->insts;
+    assert(slice.kind == KOOPA_RSIK_VALUE);
+    for (int i = 0; i < slice.len; i++) {
+        auto ptr = slice.buffer[i];
+        record_offset(reinterpret_cast<koopa_raw_value_t>(ptr));
+    }
+}
+
+// 函数
+void KoopaParser::record_offset(const koopa_raw_function_t &func) {
+    // 访问所有基本块
+    auto &slice = func->bbs;
+    assert(slice.kind == KOOPA_RSIK_BASIC_BLOCK);
+    for (int i = 0; i < slice.len; i++) {
+        auto ptr = slice.buffer[i];
+        record_offset(reinterpret_cast<koopa_raw_basic_block_t>(ptr));
+    }
+}
+
+// 加载值到寄存器
+void KoopaParser::load_value(const koopa_raw_value_t &value, const std::string &reg) {
+    // 根据指令类型判断后续需要如何访问
+    auto &kind = value->kind;
+    switch (kind.tag) {
+        case KOOPA_RVT_INTEGER:
+            // 整数指令
+            o << "li " << reg << ", " << kind.data.integer.value << std::endl;
+            break;
+        default:
+            // 其他类型从栈中加载
+            o << "lw " << reg << ", " << get_offset(value) << "(sp)" << std::endl;
+    }
+}
+
+// 保存值到栈
+void KoopaParser::store_value(const koopa_raw_value_t &value, const std::string &reg) {
+    // 根据指令类型判断后续需要如何访问
+    auto &kind = value->kind;
+    switch (kind.tag) {
+        case KOOPA_RVT_INTEGER:
+            // 整数指令
+            assert(false);
+            break;
+        default:
+            // 其他类型保存到栈中
+            o << "sw " << reg << ", " << get_offset(value) << "(sp)" << std::endl;
+    }
+}
+
+// 编译
 
 // 程序
 void KoopaParser::Compile(const koopa_raw_program_t &program) {
@@ -130,8 +217,18 @@ void KoopaParser::Compile(const koopa_raw_function_t &func) {
     o << ".globl " << real_name(func) << std::endl;
     // 标记入口
     o << real_name(func) << ":" << std::endl;
+    // 遍历并记录栈偏移量
+    stack_length = 0;
+    record_offset(func);
+    // 栈偏移量以16字节为单位向上取值
+    int round_up = 16;
+    stack_length = (stack_length + round_up - 1) / round_up * round_up;
+    assert(stack_length < 0x800 && stack_length >= -0x800 && "stack overflow");
+    // 保存栈帧
+    o << "addi sp, sp, " << -stack_length << std::endl;
     // 编译所有基本块
     Compile(func->bbs);
+    // 恢复栈帧由返回指令完成
 }
 
 // 基本块
@@ -141,39 +238,98 @@ void KoopaParser::Compile(const koopa_raw_basic_block_t &bb) {
 }
 
 // 指令
-// 子情况声明
-void CompileReturn(const koopa_raw_value_kind_t &kind, std::ostream &o);
-void CompileInteger(const koopa_raw_value_kind_t &kind, std::ostream &o);
-
 void KoopaParser::Compile(const koopa_raw_value_t &value) {
     // 根据指令类型判断后续需要如何访问
     auto &kind = value->kind;
     switch (kind.tag) {
         case KOOPA_RVT_RETURN:
             // 返回指令
-            CompileReturn(kind, o);
+            CompileReturn(value);
             break;
         case KOOPA_RVT_INTEGER:
             // 整数指令
-            CompileInteger(kind, o);
+            o << kind.data.integer.value;
+            break;
+        case KOOPA_RVT_BINARY:
+            // 二元指令
+            CompileBinary(value);
             break;
         default:
             // 其他类型暂时遇不到
+            debug(kind.tag);
             assert(false);
     }
 }
 
-void CompileReturn(const koopa_raw_value_kind_t &kind, std::ostream &o) {
+void KoopaParser::CompileReturn(const koopa_raw_value_t &value) {
+    debug("CompileReturn");
+    const koopa_raw_value_kind_t &kind = value->kind;
     auto &ret = kind.data.ret.value;
     if (ret) {
-        o << "li a0, ";
-        CompileInteger(ret->kind, o);
-        o << std::endl;
+        load_value(ret, "a0");
     }
+    o << "addi sp, sp, " << stack_length << std::endl;
     o << "ret" << std::endl;
 }
 
-
-void CompileInteger(const koopa_raw_value_kind_t &kind, std::ostream &o) {
-    o << kind.data.integer.value;
+void KoopaParser::CompileBinary(const koopa_raw_value_t &value) {
+    debug("CompileBinary");
+    const koopa_raw_value_kind_t &kind = value->kind;
+    auto &bin = kind.data.binary;
+    auto &lhs = bin.lhs;
+    auto &rhs = bin.rhs;
+    load_value(lhs, "t0");
+    load_value(rhs, "t1");
+    switch (bin.op) {
+        case KOOPA_RBO_ADD:
+            o << "add t0, t0, t1" << std::endl;
+            break;
+        case KOOPA_RBO_SUB:
+            o << "sub t0, t0, t1" << std::endl;
+            break;
+        case KOOPA_RBO_MUL:
+            o << "mul t0, t0, t1" << std::endl;
+            break;
+        case KOOPA_RBO_DIV:
+            o << "div t0, t0, t1" << std::endl;
+            break;
+        case KOOPA_RBO_MOD:
+            o << "rem t0, t0, t1" << std::endl;
+            break;
+        case KOOPA_RBO_EQ:
+            o << "sub t0, t0, t1" << std::endl;
+            o << "seqz t0, t0" << std::endl;
+            break;
+        case KOOPA_RBO_NOT_EQ:
+            o << "sub t0, t0, t1" << std::endl;
+            o << "snez t0, t0" << std::endl;
+            break;
+        case KOOPA_RBO_GT:
+            o << "slt t0, t1, t0" << std::endl;
+            break;
+        case KOOPA_RBO_LT:
+            o << "slt t0, t0, t1" << std::endl;
+            break;
+        case KOOPA_RBO_GE:
+            o << "slt t0, t0, t1" << std::endl;
+            o << "seqz t0, t0" << std::endl;
+            break;
+        case KOOPA_RBO_LE:
+            o << "slt t0, t1, t0" << std::endl;
+            o << "seqz t0, t0" << std::endl;
+            break;
+        case KOOPA_RBO_AND:
+            o << "and t0, t0, t1" << std::endl;
+            break;
+        case KOOPA_RBO_OR:
+            o << "or t0, t0, t1" << std::endl;
+            break;
+        case KOOPA_RBO_XOR:
+            o << "xor t0, t0, t1" << std::endl;
+            break;
+        default:
+            // 不支持移位, SysY生成的代码不会遇到
+            assert(false);
+    }
+    store_value(value, "t0");
 }
--- src/koopa_parser.h
+++ src/koopa_parser.h
@@ -2,11 +2,15 @@
 #define __KOOPA_PARSER_H__
 
 #include <iostream>
+#include <unordered_map>
+#include <string>
 #include "koopa.h"
 
 class KoopaParser {
 private:
     int indent_level = 0;
+    int stack_length = 0;
+    std::unordered_map<koopa_raw_value_t, int> value_map;
     std::ostream &o;
 
 public:
@@ -19,11 +23,23 @@ public:
     void Visit(const koopa_raw_basic_block_t &);
     void Visit(const koopa_raw_value_t &);
 
+    int get_offset(const koopa_raw_value_t &);
+    void set_offset(const koopa_raw_value_t &);
+    void record_offset(const koopa_raw_value_t &);
+    void record_offset(const koopa_raw_basic_block_t &);
+    void record_offset(const koopa_raw_function_t &);
+
+    void load_value(const koopa_raw_value_t &, const std::string&);
+    void store_value(const koopa_raw_value_t &, const std::string&);
+
     void Compile(const koopa_raw_program_t &);
     void Compile(const koopa_raw_slice_t &);
     void Compile(const koopa_raw_function_t &);
     void Compile(const koopa_raw_basic_block_t &);
     void Compile(const koopa_raw_value_t &);
+
+    void CompileReturn(const koopa_raw_value_t &);
+    void CompileBinary(const koopa_raw_value_t &);
 };
 
 #endif
